915005dfe30d464f2237dcff645cd28750141586
Mateo <mateo.lostanlen@gmail.com>
2023-07-03 23:17:05 +0200

    fix link

--- app/pages/04_thanks.py
+++ app/pages/04_thanks.py
@@ -2,7 +2,6 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
-from utils import get_ip
 
 st.set_page_config(
     page_title="Pyronear Annotation Tool",
@@ -11,19 +10,19 @@ st.set_page_config(
 
 st.image("logo.png", use_column_width=True)
 
+load_dotenv(".env")
+cvat_url = os.environ.get("HOST")
+streamlit_url = cvat_url.replace("8080", "8501")
+
 
 st.subheader("💪 Un grand merci pour ta participation !")
-
-url = f"http://{get_ip()}:8501/#on-a-besoin-de-tes-yeux-pour-d-tecter-des-d-parts-de-feux"
 
 st.write("N'hésite pas à partager l'initiative à tes proches en copiant-collant le message ci-après :")
 st.write(
     f"""Tu as un ordi et 15mn de dispo ? L'ONG [Pyronear](https://pyronear.org/) a besoin de tes yeux pour
-détecter des départs de feux et alerter les pompiers dès que possible. Rdv sur ce [lien]({url})"""
+détecter des départs de feux et alerter les pompiers dès que possible. Rdv sur ce [lien]({streamlit_url})"""
 )
 
-load_dotenv(".env")
-cvat_url = os.environ.get("HOST")
 
 st.subheader("☝️ Tu es dispo 15 minutes de plus ?")
 st.write(f"Clique à nouveau sur [ce lien]({cvat_url}) pour labeliser d’autres images")
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,3 @@
-import socket
-
-import streamlit as st
 from streamlit.components.v1 import html
 
 
@@ -31,28 +28,3 @@ def nav_page(page_name, timeout_secs=3):
         timeout_secs,
     )
     html(nav_script)
-
-
-def redirect_button(url: str, text: str = None, color="#FD504D"):
-    st.markdown(
-        f"""
-    <a href="{url}" target="_self">
-        <div style="
-            display: inline-block;
-            padding: 0.5em 1em;
-            color: #FFFFFF;
-            background-color: {color};
-            border-radius: 3px;
-            text-decoration: none;">
-            {text}
-        </div>
-    </a>
-    """,
-        unsafe_allow_html=True,
-    )
-
-
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
