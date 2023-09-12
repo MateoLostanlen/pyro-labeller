8b79bf4438bc07170e10341bc48e7654f15436ce
Mateo <mateo.lostanlen@gmail.com>
2023-09-12 10:12:28 +0200

    must be different

--- app/pages/03_start.py
+++ app/pages/03_start.py
@@ -13,7 +13,7 @@ def get_credentials():
     if len(df_free) > 0:
         data = df_free.iloc[0]
         df.loc[df["username"] == data.username, ["assign_time"]] = datetime.now()
-        if data.username == "username":
+        if data.username != "username":
             df.to_csv("data/task_database.csv")
             return data.username, data.password
 
